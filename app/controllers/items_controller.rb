350a2a3de212f9fb8efb533d94ac4d39c5a53118
camryn williams <williams.camryn.e@gmail.com>
2021-06-27 21:20:03 -0400

    commit tree 7a2f4b6af2fcf922e14304ea45667fedfb28710f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RoutingError, with: :render_bad_route_response
+
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, include: :user, status: :created
+    puts item.name
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  # def render_bad_route_response
+  #   render json: { error: "Route not found" }, status: :not_found
+  # end
 
 end
